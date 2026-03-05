18ab0deb702e7514c79cdfeb9b45634a4bd365b9
WXJDI <wajdibenouirane@gmail.com>
2026-03-05 21:49:55 +0100

    Commit initial : Architecture du Projet en suivant le pattern TDD

---
+++ CantineKata.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CantineKata.Core;
+
+public class Class1
+{
+
+}
---
+++ CantineKata.Tests/CalculPrixTests.cs
@@ -0,0 +1,10 @@
+namespace CantineKata.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
